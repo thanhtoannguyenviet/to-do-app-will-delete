0046e9641adf35800f590c8d5c01bbfacc859549
Nguyễn Viết Thanh Toàn <thanhtoan.nguyenviet@gmail.com>
2025-03-19 19:29:58 +0700

    session-3-model

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,15 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
-
+// Tạo data giả
+// xóa list Tasks -> 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::latest('title')->get();
+    return view('index',['tasks'=>$tasks]);
 });
 
-Route::get('/greeting', function () {
-    return 'Hello World';
-})->name('greeting'); // name of route
-Route::get('/greeting/{name}', function ($name) {
-    return 'Hello ' . $name;
-});
-Route::get('/hiU', function () {
-    return redirect('/greeting');
-});
-Route::get('/hi2', function () {
-    return redirect()->route('greeting'); // redirect to route has name 'greeting'
-});
-Route::get('/Home', function () {
-    return redirect('/'); 
-});
+Route::get('/tasks/{id}', function ($id) {  
+    $task = Task::findOrFail($id);
+    return view('detail',['task'=>$task]);
+})->name('tasks.detail');
