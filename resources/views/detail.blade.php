9af7c08c96347d3886ee1581d14ecf5e3044c8e7
Nguyễn Viết Thanh Toàn <thanhtoan.nguyenviet@gmail.com>
2025-03-21 18:45:21 +0700

    session-4-model

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{$task->title}}</title>
-</head>
-<body>
-    <h1>{{$task->title}}</h1>
+@extends('layouts.app')
+@section('title',$task->title)
+@section('content')
     <p>{{$task->description}}</p>
     <p>{{$task->long_description}}</p>
     <p>{{$task->completed ? 'Đã hoàn thành' : 'Chưa hoàn thành'}}</p>
     <a href="/">Quay lại</a>
-</body>
-</html>
+@endsection
